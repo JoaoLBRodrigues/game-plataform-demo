648aec1865e61ffdac9621a55e11e498a999a682
João Rodrigues <joao.rodrigues@infracontrol.com>
2025-08-17 10:57:47 +0100

    center loading

--- app/src/components/GameList.tsx
+++ app/src/components/GameList.tsx
@@ -1,37 +1,52 @@
-import { Grid, Typography } from "@mui/material";
+import { Box, CircularProgress, Grid, Typography } from "@mui/material";
 import GameCard, { type Props } from "./GameCard";
 import { useGames } from "../hooks/useGames";
 import GenreLIst from "./GenreLIst";
 
 export default function GameList() {
-	const { error, data } = useGames();
+	const { error, data, isLoading } = useGames();
 	return (
-		<Grid container size={12} my={5}>
-			<Typography color="red">{error}</Typography>
-			<Grid size={{ md: 1, xs: 12 }}>
-				<GenreLIst />
-			</Grid>
-			<Grid
-				size={{ xs: 12, md: 11 }}
-				sx={{
-					display: "flex",
-					justifyContent: "center",
-					alignItems: "center",
-				}}
-			>
-				<Grid container spacing={1} size={10}>
-					{data.map((item: Props, index: number) => (
-						<Grid size={{ xs: 12, md: 3 }} key={index}>
-							<GameCard
-								name={item.name}
-								background_image={item.background_image}
-								parent_platforms={item.parent_platforms}
-								metacritic={item.metacritic}
-							/>
+		<>
+			{isLoading ? (
+				<Box
+					sx={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						minHeight: "100vh",
+					}}
+				>
+					<CircularProgress />
+				</Box>
+			) : (
+				<Grid container size={12} my={5}>
+					<Typography color="red">{error}</Typography>
+					<Grid size={{ md: 1, xs: 12 }}>
+						<GenreLIst />
+					</Grid>
+					<Grid
+						size={{ xs: 12, md: 11 }}
+						sx={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+						}}
+					>
+						<Grid container spacing={1} size={10}>
+							{data.map((item: Props, index: number) => (
+								<Grid size={{ xs: 12, md: 3 }} key={index}>
+									<GameCard
+										name={item.name}
+										background_image={item.background_image}
+										parent_platforms={item.parent_platforms}
+										metacritic={item.metacritic}
+									/>
+								</Grid>
+							))}
 						</Grid>
-					))}
+					</Grid>
 				</Grid>
-			</Grid>
-		</Grid>
+			)}
+		</>
 	);
 }
